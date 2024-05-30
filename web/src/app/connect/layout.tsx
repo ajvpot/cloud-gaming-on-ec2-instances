a42d88e1c75e6c668e30103f4f7b62bf1f8339e8
Alex Vanderpot <553597+ajvpot@users.noreply.github.com>
2024-05-29 23:15:07 -0700

    recommendation

--- web/src/app/connect/layout.tsx
+++ web/src/app/connect/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ height: "100%" }}>
       <body style={{ height: "100%" }}>
         <div style={{ height: "100%" }}>{children}</div>
       </body>

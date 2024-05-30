856e641b6aed1d6785ba2e7a4caf3cc83dca17e3
Alex Vanderpot <553597+ajvpot@users.noreply.github.com>
2024-05-29 23:14:56 -0700

    recommendation

--- web/src/app/connect/layout.tsx
+++ web/src/app/connect/layout.tsx
@@ -5,7 +5,9 @@ export default function Layout({
 }>) {
   return (
     <html lang="en">
-      <body style={{ height: "100%" }}>{children}</body>
+      <body style={{ height: "100%" }}>
+        <div style={{ height: "100%" }}>{children}</div>
+      </body>
     </html>
   );
 }

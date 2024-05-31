dcd98569a2012e47f65b6987422f8166b7fa78ed
Alex Vanderpot <alex@vanderpot.com>
2024-05-30 22:15:25 -0700

    add user stack map

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,8 +1,22 @@
+import { setupDevBindings } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
         urlImports: ['https://assets-oh3.pages.dev/'],
     },
 };
+
+if (process.env.NODE_ENV === 'development') {
+    await setupDevBindings({
+        bindings: {
+            CLOUDTD_USER_STACK_MAP: {
+                type: 'kv',
+                id: '45728eeaff424d71aa6da82e47a8dc78',
+            },
+        }
+    });
+}
+
 
 export default nextConfig;
--- web/src/app/actions.ts
+++ web/src/app/actions.ts
@@ -5,10 +5,35 @@ import {
   startInstances,
   stopInstances,
 } from "../lib/ec2Actions";
+import { KVNamespace } from "@cloudflare/workers-types";
+import { headers } from "next/headers";
+
+// Assuming you have a KV namespace binding named `CLOUDTD_USER_STACK_MAP`
+const CLOUDTD_USER_STACK_MAP: KVNamespace = process.env.CLOUDTD_USER_STACK_MAP;
+
+async function getStackNameFromKV(email: string): Promise<string | null> {
+  return await CLOUDTD_USER_STACK_MAP.get(email);
+}
+
+function getEmail() {
+  if (process.env.NODE_ENV == "development") {
+    return "alex@vanderpot.com";
+  }
+  const email = headers().get("Cf-Access-Authenticated-User-Email");
+  if (!email) {
+    throw new Error("User email not found in headers");
+  }
+  return email;
+}
 
 export async function startEC2Instances(): Promise<{ message: string }> {
   try {
-    await startInstances(process.env.STACK_NAME!);
+    const stackName = await getStackNameFromKV(getEmail());
+    if (!stackName) {
+      throw new Error("Stack name not found in KV store");
+    }
+
+    await startInstances(stackName);
     return { message: "Instances started successfully" };
   } catch (error: any) {
     return { message: error.message };
@@ -17,7 +42,12 @@ export async function startEC2Instances(): Promise<{ message: string }> {
 
 export async function stopEC2Instances(): Promise<{ message: string }> {
   try {
-    await stopInstances(process.env.STACK_NAME!);
+    const stackName = await getStackNameFromKV(getEmail());
+    if (!stackName) {
+      throw new Error("Stack name not found in KV store");
+    }
+
+    await stopInstances(stackName);
     return { message: "Instances stopped successfully" };
   } catch (error: any) {
     return { message: error.message };
@@ -28,6 +58,11 @@ export async function getPassword(
   instanceId: string,
 ): Promise<{ message: string; password?: string }> {
   try {
+    const stackName = await getStackNameFromKV(getEmail());
+    if (!stackName) {
+      throw new Error("Stack name not found in KV store");
+    }
+
     return {
       message: "Password decrypted successfully",
       password: await getDecryptedPassword(instanceId),

57b10246a3b4b4c487fcffab0a34084679948212
Alex Vanderpot <alex@vanderpot.com>
2024-05-30 23:37:11 -0700

    a

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -114,9 +114,7 @@ export default function Home() {
           {statuses.map((status) => (
             <div key={status.instanceId} className="bg-gray-800 p-4 rounded-lg">
               <div className="flex items-center justify-between mb-2">
-                <h3 className="text-lg font-medium">
-                  {status.name}/{status.type}
-                </h3>
+                <h3 className="text-lg font-medium">{status.name}</h3>
                 <span
                   className={`px-2 py-1 rounded-full text-xs ${status.state === "running" ? "bg-green-500" : status.state === "stopped" ? "bg-red-500" : "bg-yellow-500"} text-white`}
                 >
@@ -124,6 +122,8 @@ export default function Home() {
                 </span>
               </div>
               <p className="text-gray-400 mb-2">
+                {status.type}
+                <br />
                 IP Address:{" "}
                 <a href={`https://${status.publicIp}:8443`}>
                   {status.publicIp}
